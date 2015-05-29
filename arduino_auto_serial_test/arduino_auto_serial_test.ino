cecb6a531e36da90de09256369aea112c34dd553
stijnvankampen <stijnvankampenmob@gmail.com>
2015-05-29 10:37:17 +0200

    servo aansturing testen

--- arduino_auto_serial_test/arduino_auto_serial_test.ino
+++ arduino_auto_serial_test/arduino_auto_serial_test.ino
@@ -1,15 +1,41 @@
+#include <Servo.h>
+
 #include <SoftwareSerial.h>
+
+Servo servo1; 
+
 bool serialConnected = false;
 int led = 13;
 int val = 0;
 
+bool test = true;
+
+int pos;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   
+  servo1.attach(2);
+  
   pinMode(led, OUTPUT);
   digitalWrite(led, LOW);
+}
+
+void heen(){
+  for(pos = 0; pos < 45; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    servo1.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(5);                       // waits 15ms for the servo to reach the position 
+  } 
+}
+
+void terug(){
+  for(pos = 45; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    servo1.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(5);                       // waits 15ms for the servo to reach the position 
+  } 
 }
 
 void loop() {
@@ -20,10 +46,15 @@ void loop() {
     
     if (val == '1')
     {
-      digitalWrite(led, HIGH);
-      //serialConnected = true;
-      delay(1000);
-      digitalWrite(led, LOW);
+      if(test == true)
+      {
+        test = false;
+        heen();
+      } else if (test == false)
+      {
+        test = true;
+        terug();
+      }
     } 
     
     
